fe270b535478da0b319051ddb19b0e2c10809814
Layla Abo Saad <layla@Laylas-MacBook-Air.local>
2025-08-03 17:24:55 +0300

    created login, register, and logout functions for the frontend

---
+++ client/actions/auth.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export async function register(prevState, formData) {
+  const data = {
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirmPassword: formData.get("confirmPassword"),
+    role: formData.get("role"),
+  };
+  console.log("data", data);
+  try {
+    const res = await axios.post(`${BASE_URL}/auth/register`, data);
+    if (res.status !== 200 && res.status !== 201) {
+      return { error: res.data?.message || "Registration failed." };
+    }
+
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+    const message = error.response?.data?.error;
+
+    return { error: message, fieldData: data };
+  }
+}
+
+export async function login(prevState, formData) {
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+  console.log("data", data);
+  try {
+    const res = await axios.post(`${BASE_URL}/auth/login`, data, {
+      withCredentials: true,
+    });
+    if (res.status !== 200 && res.status !== 201) {
+      return { error: res.data?.message || "logging in failed" };
+    }
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+    const message = error.response?.data?.error;
+    return { error: message, fieldData: data };
+  }
+}
+export async function logout() {
+  try {
+    await axios.post(`${BASE_URL}/auth/logout`, null, {
+      withCredentials: true,
+    });
+    return { success: true };
+  } catch (err) {
+    console.error("Logout failed:", err);
+    return { success: false };
+  }
+}
